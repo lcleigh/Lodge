d5f384106e95f987c2b974f088d76734883d1206
Lovelace <lmkcheung02@yahoo.co.uk>
2022-02-05 20:14:40 +0000

    Add view.py

--- Lodge/views.py
+++ Lodge/views.py
@@ -1,24 +1,41 @@
+from asyncio.windows_events import NULL
 from django import forms
 from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Booking, Checkin, Room
+from .models import Booking, Checked_in_guest, Checkin, Room
 import datetime
+
+
+#tasks = ["foo", "bar","baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
+    if "lodge" not in request.session:
+        request.session["lodge"] = []
     return render(request, "lodge/index.html", {
         "rooms": Room.objects.all(),
-        "bookings": Booking.objects.all()
+        "bookings": Booking.objects.all(),
+        "checked_in_guests": Checked_in_guest.objects.all(),
+        "lodge": request.session["lodge"]   
     })
-
 
 def checkin(request):
     now = datetime.datetime.now()
     return render(request, "lodge/checkin.html", {
-        "bookings": Booking.objects.filter(first_date=now)
-        #"checkins": Checkin.objects.all()
+        "checkins": Checkin.objects.filter(checkin_date__isnull=True),
+        "bookings": Booking.objects.filter(first_date=now),   
+        "checked_in_guests": Checked_in_guest.objects.all()      
+    })
+    
+def checked_in_guests(request):   
+    return render(request, "lodge/checkin.html", {
+        "checked_in_guests": Checked_in_guest.objects.all()     
     })
 
 
@@ -46,3 +63,19 @@ def book(request, flight_id):
             return HttpResponseBadRequest("Bad Request: passenger does not exist")
         passenger.flights.add(flight)
         return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["lodge"] += [task]
+            return HttpResponseRedirect(reverse("lodge:index"))
+        else:
+            return render(request, "lodge/checkin.html", {
+                "form": form
+            })
+    else:
+        return render(request, "lodge/checkin.html", {
+            "form": NewTaskForm()
+        })
