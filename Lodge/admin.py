f06da03e21a78911d6e0a681bbb692dfa483bac8
oogie-s <95941798+oogie-s@users.noreply.github.com>
2022-02-07 20:51:11 +0000

    Updating models

--- Lodge/admin.py
+++ Lodge/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Action, Role, User, Customer, Room_type, Room, Booking, Booking_room, Checkin, Checkout, Discount_type, Seasonal_discount, Bulk_discount, Checked_in_guest
+from .models import Actions, Bookings, Roles, Users, Customers, Room_type, Rooms, Booking_rooms, Checkin, Checkout, Discount_type, Seasonal_discounts, Bulk_discounts
 
 class Room_typeAdmin(admin.ModelAdmin):
     list_display = ("__str__", "room_type_desc" )
@@ -10,18 +10,16 @@ class RoomAdmin(admin.ModelAdmin):
     list_display = ("__str__", "room_type", "sleeps")
 
 
-admin.site.register(Action)
-admin.site.register(Role)
-admin.site.register(User)
-admin.site.register(Customer)
+admin.site.register(Actions)
+admin.site.register(Roles)
+admin.site.register(Users)
+admin.site.register(Customers)
 admin.site.register(Room_type, Room_typeAdmin)
-admin.site.register(Room, RoomAdmin)
-admin.site.register(Booking)
-admin.site.register(Booking_room)
+admin.site.register(Rooms, RoomAdmin)
+admin.site.register(Bookings)
+admin.site.register(Booking_rooms)
 admin.site.register(Checkin)
 admin.site.register(Checkout)
 admin.site.register(Discount_type)
-admin.site.register(Seasonal_discount)
-admin.site.register(Bulk_discount)
-admin.site.register(Checked_in_guest)
-
+admin.site.register(Seasonal_discounts)
+admin.site.register(Bulk_discounts)
--- Lodge/urls.py
+++ Lodge/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("checkin", views.checkin, name="checkin"),
-    path("<int:flight_id>/book", views.book, name="book")
-]
+    path("", views.index),
+    path("checkin", views.checkin),]
